a641d2c18057b2ef1293d6d33eff81a4844100fe
etet100 <wokuukow@o2.pl>
2025-10-11 15:09:21 +0200

    Wstępna obsługa kierunkowskazów

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+// Koniecznie trzeba opisać co oznaczają te OUT i BTN, a jeszcze lepiej je ponazywać
+// np. OUT1_PIN -> PIN_HEADLIGHT, BTN1 -> BTN_HEADLIGHT
 #define OUT1_PIN 2
 #define OUT2_PIN 3
 #define OUT3_PIN 4
@@ -6,6 +8,7 @@
 #define OUT6_PIN 7
 #define OUT7_PIN 8
 #define OUT8_PIN 9
+
 #define BUTTON_PIN 0
 
 #define BTN_NONE 0
@@ -26,14 +29,15 @@ int getBtn() {
   buttonValue = analogRead(BUTTON_PIN);
 //  Serial.println(buttonValue);
 
-  if (buttonValue == 0) {return BTN1;}
-  if (buttonValue > 4 && buttonValue < 8) {return BTN2;}
-  if (buttonValue > 22 && buttonValue < 28) {return BTN3;}
-  if (buttonValue > 8 && buttonValue < 13) {return BTN4;}
-  if (buttonValue > 52 && buttonValue < 57) {return BTN5;}
-  if (buttonValue > 129 && buttonValue < 134) {return BTN6;}
-//  if (buttonValue > 80 && buttonValue < 85) {return BTN7;}
-//  if (buttonValue > 15 && buttonValue < 20) {return BTN8;}
+  if (buttonValue == 0) { return BTN_NONE; }
+  if (buttonValue > 4 && buttonValue < 8) { return BTN2; }
+  if (buttonValue > 22 && buttonValue < 28) { return BTN3; }
+  if (buttonValue > 8 && buttonValue < 13) { return BTN4; }
+  if (buttonValue > 52 && buttonValue < 57) { return BTN5; }
+  if (buttonValue > 129 && buttonValue < 134) { return BTN6; }
+  if (buttonValue > 80 && buttonValue < 85) { return BTN7; }
+  if (buttonValue > 15 && buttonValue < 20) { return BTN8; }
+
   return BTN_NONE;
 }
 
@@ -47,38 +51,48 @@ void loop() {
     if (newBtn != btn) {
       // jeśli do tej pory jakiś przycisk był wciśnięty
       if (btn != BTN_NONE) {
+        // obsługa wciśnięcia przycisku
+
         // wciśnięty powyżej sekundy???
         if (millis() - start > 1000) {
           Serial.print("LONG ");
           Serial.println(btn);
           // tutaj można dodać kod obsługi długiego wciśnięcia
-        } else
-         if (millis() - start > 100) {
-            Serial.print("SHORT ");
-            Serial.println(btn);
-            // tutaj można dodać kod obsługi krótkiego wciśnięcia
-//            if (BTN1 == SHORT 1) {digitalWrite(out1Pin,HIGH);}
+          // ...
+        } else if (millis() - start > 50) {
+          // zakładam, że krótkie wciśnięcie to powyżej 50ms, może wyeliminujemy ewentualne przypadkowe
+          // naciśnięcia, tu obsłużmy między innymi załączanie kierunkowskazu
+
+          switch (btn) {
+            case BTN7:
+              // włącz kierunkowskaz 1 i dla pewności wyłącz 2
+              digitalWrite(OUT8_PIN, LOW);
+              digitalWrite(OUT7_PIN, HIGH);
+              break;
+            case BTN8:
+              // włącz kierunkowskaz 2 i dla pewności wyłącz 1
+              digitalWrite(OUT7_PIN, LOW);
+              digitalWrite(OUT8_PIN, HIGH);
+              break;
+          }
         }
-        else {
-          Serial.print("TOO SHORT ");
-          Serial.println(btn);
+      } else {
+        // tu obsługa puszczenia przycisku, np kierunkowskazów
+        switch (btn) {
+          case BTN7:
+          case BTN8:
+            // wyłączamy dla pewności oba kierunkowskazy
+            digitalWrite(OUT7_PIN, LOW);
+            digitalWrite(OUT8_PIN, LOW);
+            break;
         }
       }
+
       // zapamiętujemy aktualny czas, jeśli przycisk się zmieni to
       // będziemy mogli policzyć czas jego wciśnięcia
       start = millis();
       // zapamiętujemy nowy stan jako bieżący stan
       btn = newBtn;
-    }
-    if (buttonValue > 81 && buttonValue < 84) {
-      digitalWrite(OUT7_PIN, HIGH);
-    } else {
-      digitalWrite(OUT7_PIN, LOW);
-    }
-    if (buttonValue > 16 && buttonValue < 18) {
-      digitalWrite(OUT8_PIN, HIGH);
-    } else {
-      digitalWrite(OUT8_PIN, LOW);
     }
   }
 }
