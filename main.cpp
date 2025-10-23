37d521caff8acfc74ce7d6c56151358cf2c880d6
WojtekGD <46536554+WojtekGD@users.noreply.github.com>
2025-10-23 22:51:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,148 +1,122 @@
+//spora rewolucja
+#include <AnalogButtons.h>
+#ifndef A1 // esp32 doesn't define analog pins
+#define ANALOG_PIN 0
+#else
+#define ANALOG_PIN A0
+#endif
 
-#include <Arduino.h>
-#include "definitions.h"
+int out1 = 2;
+int out2 = 3;
+int out3 = 4;
+int out4 = 5;
+int out5 = 6;
+int out6 = 7;
+int out7 = 8;
+int out8 = 9;
+int wh_s = 10;
 
+// A call back function that you pass into the constructor of AnalogButtons, see example
+// below. Alternativly you could extend the Button class and re-define the methods pressed() 
+// or held() which are called 
+void b1Click() {  
+    Serial.print("B1_click"); 
+}
+void b1Hold() {  
+    Serial.print("B1_hold"); 
+}
+void b2Click() {  
+    Serial.print("B2_click"); 
+}
+void b2Hold() {  
+    Serial.print("B2_hold"); 
+}
+
+void b3Click() {  
+    Serial.print("B3_click"); 
+}
+void b3Hold() {  
+    Serial.print("B3_hold"); 
+}
+
+void b4Click() {  
+    Serial.print("B4_click"); 
+}
+void b4Hold() {  
+    Serial.print("B4_hold"); 
+}
+
+void b5Click() {  
+    Serial.print("B5_click"); 
+}
+void b5Hold() {  
+    Serial.print("B5_hold"); 
+}
+
+void b6Click() {  
+    Serial.print("B6_click"); 
+}
+void b6Hold() {  
+    Serial.print("B6_hold"); 
+}
+
+void b7Click() {  
+    Serial.print("B7_click"); 
+}
+void b7Hold() {  
+    Serial.print("B7_hold"); 
+}
+
+void b8Click() {  
+    Serial.print("B8_click"); 
+}
+void b8Hold() {  
+    Serial.print("B8_hold"); 
+}
+
+
+
+AnalogButtons analogButtons(ANALOG_PIN, INPUT, 5, 3);
+Button b1 = Button(0, &b1Click, &b1Hold, 1000, 2500);
+Button b2 = Button(6, &b2Click, &b2Hold, 1000, 2500);
+Button b3 = Button(26, &b3Click, &b3Hold, 1000, 2500);
+Button b4 = Button(11, &b4Click, &b4Hold, 1000, 2500);
+Button b5 = Button(132, &b5Click, &b5Hold, 1000, 2500);
+Button b6 = Button(55, &b6Click, &b6Hold, 1000, 2500);
+Button b7 = Button(83, &b7Click, &b7Hold, 1000, 2500);
+Button b8 = Button(17, &b8Click, &b8Hold, 1000, 2500);
 void setup() {
   Serial.begin(9600);
+//  Serial.println("Testing your Analog buttons");
 
-  pinMode(OUTPUT_TURN_LEFT, OUTPUT);
-  pinMode(OUTPUT_TURN_RIGHT, OUTPUT);
-  pinMode(INPUT_WHEEL_ROT_SENSOR, INPUT_PULLUP);
-  // warto ustawić resztę wyjść i wejść
+  analogButtons.add(b1);
+  analogButtons.add(b2);
+  analogButtons.add(b3);
+  analogButtons.add(b4);
+  analogButtons.add(b5);
+  analogButtons.add(b6);
+  analogButtons.add(b7);
+  analogButtons.add(b8);
+
+// out1-left out2-right turn relays
+// out3
+// out4 
+// out5 
+// out6 
+  pinMode(out1, OUTPUT);
+  pinMode(out2, OUTPUT);
+  pinMode(wh_s, INPUT_PULLUP); 
+ }
+ 
+void loop() {  
+  // To check values when button are pressed
+  analogButtons.check();
+  
+
 }
 
-int getBtn() {
-  int adcValue = analogRead(BUTTON_PIN);
-
-  if (adcValue > BTN_TURN_LEFT_ADC_MIN && adcValue < BTN_TURN_LEFT_ADC_MAX) { return BTN_TURN_LEFT; }
-  else if (adcValue > BTN_TURN_RIGHT_ADC_MIN && adcValue < BTN_TURN_RIGHT_ADC_MAX) { return BTN_TURN_RIGHT; }
-  else if (adcValue > 22 && adcValue < 28) { return BTN3; }
-  else if (adcValue > 8 && adcValue < 13) { return BTN4; }
-  else if (adcValue > 52 && adcValue < 57) { return BTN5; }
-  else if (adcValue > 129 && adcValue < 134) { return BTN6; }
-  else if (adcValue > 80 && adcValue < 85) { return BTN7; }
-  else if (adcValue > 15 && adcValue < 20) { return BTN8; }
-
-  return BTN_NONE;
-}
-
-// funkcja zwraca nowy przycisk dopiero kiedy jego stan się ustabilizuje
-// czyli nie zmieni przez 50ms (debouncing)
-int getDebouncedBtn() {
-  // to jest stan "stały", czyli po deobuncingu (min 50ms), static to zmienne których wartość
-  // jest zachowywana pomiędzy wywołaniami funkcji
-  static int currentButton = BTN_NONE;
-  // tu będziemy zapisywać stan "chwilowy", taki stan przechodzi
-  // do stanu stałego jeśli się nie zmieni przez 50ms (debouncing)
-  static int debouncingButton = 0;
-  static unsigned long firstSeen = 0;
-
-  int buttonValue = getBtn();
-  if (buttonValue != debouncingButton) {
-    // zmiana przycisku, resetujemy licznik czasu
-    firstSeen = millis();
-    debouncingButton = buttonValue;
-    // nie uzysknęliśmy jeszcze stabilnego stanu
-  } else if (millis() - firstSeen > 50) {
-    // minęło 50ms, przycisk się nie zmienił, więc
-    // uzyskaliśmy stabilny stan
-    currentButton = debouncingButton;
-  }
-
-  return currentButton;
-}
-
-void loop() {
-  static int btn = BTN_NONE;
-  static unsigned long start = millis();
-  static unsigned long turnLeftStopTime = 0;
-  static unsigned long turnRightStopTime = 0;
-
-    // tu w pętli sprawdzamy stan przycisków i czekamy na zmianę
-    int newBtn = getDebouncedBtn();
-    // wykrywajmy zmianę stanu przycisków
-    if (newBtn != btn) {
-      Serial.print("Button changed from ");
-      Serial.print(btn);
-      Serial.print(" to ");
-      Serial.println(newBtn);
-
-      // jeśli do tej pory jakiś przycisk był wciśnięty
-      if (btn != BTN_NONE) {
-        // obsługa wciśnięcia przycisku
-
-        // wciśnięty powyżej sekundy???
-        if (millis() - start > 1000) {
-          // obsługa długiego wciśnięcia
-          switch (btn) {
-            case BTN_TURN_LEFT:
-              // po prostu włącz
-              digitalWrite(OUTPUT_TURN_LEFT, HIGH);
-              break;
-          }
-        } else if (millis() - start > 150) {
-          // obsługa krótkiego wciśnięcia
-          switch (btn) {
-            case BTN_TURN_LEFT:
-              // jeśli którykolwiek z kierunkowskazów jest włączony to go wyłącz, w przeciwnym razie włącz LEWY
-              if (digitalRead(OUTPUT_TURN_LEFT) == LOW || digitalRead(OUTPUT_TURN_RIGHT) == LOW) {
-                digitalWrite(OUTPUT_TURN_LEFT, LOW);
-                digitalWrite(OUTPUT_TURN_LEFT, LOW);
-              } else {
-                digitalWrite(OUTPUT_TURN_LEFT, HIGH);
-                turnLeftStopTime = 0; // deaktywuj timer
-              }
-              break;
-          }
-
-          // zakładam, że krótkie wciśnięcie to powyżej 50ms, może wyeliminujemy ewentualne przypadkowe
-          // naciśnięcia, tu obsłużmy między innymi załączanie kierunkowskazu
-
-          // switch (btn) {
-          //   case BTN7:
-          //     // włącz kierunkowskaz 1 i dla pewności wyłącz 2
-          //     digitalWrite(OUT8_PIN, LOW);
-          //     digitalWrite(OUT7_PIN, HIGH);
-          //     break;
-          //   case BTN8:
-          //     // włącz kierunkowskaz 2 i dla pewności wyłącz 1
-          //     digitalWrite(OUT7_PIN, LOW);
-          //     digitalWrite(OUT8_PIN, HIGH);
-          //     break;
-          // }
-        }
-      } else {
-        // tu obsługa puszczenia przycisku, np kierunkowskazów
-        // switch (btn) {
-        //   case BTN7:
-        //   case BTN8:
-        //     // wyłączamy dla pewności oba kierunkowskazy
-        //     digitalWrite(OUT7_PIN, LOW);
-        //     digitalWrite(OUT8_PIN, LOW);
-        //     break;
-        // }
-      }
-
-      // zapamiętujemy aktualny czas, jeśli przycisk się zmieni to
-      // będziemy mogli policzyć czas jego wciśnięcia
-      start = millis();
-      // zapamiętujemy nowy stan jako bieżący stan
-      btn = newBtn;
-    }
-
-    if (turnLeftStopTime != 0 && millis() > turnLeftStopTime) {
-      // czas wyłączenia minął, wyłączamy kierunkowskaz
-      digitalWrite(OUTPUT_TURN_LEFT, LOW);
-      turnLeftStopTime = 0; // deaktywuj timer
-    } else if (turnLeftStopTime == 0 && digitalRead(OUTPUT_TURN_LEFT) == HIGH && digitalRead(INPUT_WHEEL_ROT_SENSOR) == LOW) {
-      // kierunkowskaz włączon długim wciśnięciem, czujnik wykrył obrót koła, wyłącz za sekundę
-      turnLeftStopTime = millis() + 1000;
-    }
-
-    // if (turnRightStopTime != 0 && millis() > turnRightStopTime) {
-    //   // czas wyłączenia minął, wyłączamy kierunkowskaz
-    //   digitalWrite(OUTPUT_TURN_RIGHT, LOW);
-    //   turnRightStopTime = 0; // deaktywuj timer
-    // }
-  //}
+void configure() {
+  unsigned int value = analogRead(ANALOG_PIN);
+  Serial.println(value);
+  delay(250);
 }
